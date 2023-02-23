42e6a0a619d5efb7fd11d7b8b50c81ed8734baf4
gurkan-lite <117160581+gurkan-lite@users.noreply.github.com>
2023-02-23 12:10:37 +0100

    first

---
+++ lunar.js
@@ -0,0 +1,20 @@
+// world
+function world() {
+  push();
+  noStroke();
+  fill(208, 213, 255);
+  rect(1, 1, 1000, 1000);
+}
+
+// groke
+// function groke(x, y) {
+//   push();
+//   translate(x, y);
+//   noStroke();
+//   fill(200, 205, 233);
+//   rect(75, 50, 150, 150);
+// }
+
+function draw() {
+  world(1, 1);
+}
